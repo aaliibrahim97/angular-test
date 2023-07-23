4e07bcb9518effcb6e2db6d537918eb79598ea25
aaliibrahima97 <aaliibrahima97@gmail.com>
2023-07-23 22:23:56 +0400

    Update components names + bug fix

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import {provideHttpClient} from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {VERSION as CDK_VERSION} from '@angular/cdk';
 import {VERSION as MAT_VERSION, MatNativeDateModule} from '@angular/material/core';
-import {TableOverviewExampleComponent} from './example/table-overview-example';
+import { MainListComponent } from './app/main-list';
 
 /* eslint-disable no-console */
 console.info('Angular CDK version', CDK_VERSION.full);
 console.info('Angular Material version', MAT_VERSION.full);
 
-bootstrapApplication(TableOverviewExampleComponent, {
+bootstrapApplication(MainListComponent, {
   providers: [
     provideAnimations(),
     provideHttpClient(),
